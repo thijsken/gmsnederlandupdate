8499c7e13e3e3fa3c6a75ef263d2ef8dd4a41819
thijsken <41241417+thijsken@users.noreply.github.com>
2025-06-02 10:21:37 +0200

    fix server.js

--- server.js
+++ server.js
@@ -20,8 +20,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './public')));
 
-localStorage.setItem('serverId', serverId);
-
 // 🧠 Tijdelijke opslag
 let meldingen = [];
 let eenheden = [];
