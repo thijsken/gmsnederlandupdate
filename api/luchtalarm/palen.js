06de94960d2c0f0a25feb37f35730279d25e60d7
thijsken <41241417+thijsken@users.noreply.github.com>
2025-06-10 10:30:15 +0200

    update luchtalarm/api

--- api/luchtalarm/palen.js
+++ api/luchtalarm/palen.js
@@ -1,7 +1,13 @@
 export default async function handler(req, res) {
   if (req.method === 'GET') {
-    // TODO: laad paaldata
     return res.status(200).json([]);
+  }
+
+  if (req.method === 'POST') {
+    const body = req.body;
+    console.log('📥 Nieuwe paaldata ontvangen:', body);
+    // TODO: sla op in database of log het ergens
+    return res.status(200).json({ message: 'Paaldata ontvangen', data: body });
   }
 
   res.status(405).json({ error: 'Method not allowed' });
