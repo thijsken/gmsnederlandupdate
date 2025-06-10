e7970e38bea5a31114ca3d3e708f9cd904e80843
thijsken <41241417+thijsken@users.noreply.github.com>
2025-06-10 10:21:17 +0200

    update meldingen.js

--- api/meldingen.js
+++ api/meldingen.js
@@ -1,16 +1,15 @@
-export async function meldingenHandler(req, res) {
-  if (req.method === 'GET') {
-    return res.status(200).json([
-      { id: 1, type: 'Brand', location: 'Utrecht', status: 'new' },
-      { id: 2, type: 'Ongeval', location: 'Rotterdam', status: 'accepted' }
-    ]);
-  }
+export default async function handler(req, res) {
+  switch (req.method) {
+    case 'GET':
+      // TODO: haal meldingen uit database
+      return res.status(200).json([]);
 
-  if (req.method === 'POST') {
-    const melding = req.body;
-    // In echte app: opslaan in DB
-    return res.status(201).json({ message: 'Melding opgeslagen', data: melding });
-  }
+    case 'POST':
+      const melding = req.body;
+      // TODO: opslaan in database
+      return res.status(201).json({ message: 'Melding opgeslagen', data: melding });
 
-  res.status(405).json({ error: 'Method not allowed' });
+    default:
+      res.status(405).json({ error: 'Method not allowed' });
+  }
 }
