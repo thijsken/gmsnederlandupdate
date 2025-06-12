b763b4499fecb7fda5b0a7e4244c5687ef056eb9
thijsken <41241417+thijsken@users.noreply.github.com>
2025-06-12 14:01:44 +0200

    alarmpaosten

--- api/posten/alarm.js
+++ api/posten/alarm.js
@@ -1,8 +1,20 @@
+let postenAlarms = []; // tijdelijke opslag van postenalarms
+
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const alarm = req.body;
-    // TODO: verwerk alarm voor post
-    return res.status(200).json({ message: 'Alarm ontvangen', data: alarm });
+
+    // Optioneel: validatie van alarm-object
+    if (!alarm || typeof alarm !== 'object') {
+      return res.status(400).json({ error: 'Ongeldige payload' });
+    }
+
+    // Voeg alarm toe aan opslag
+    postenAlarms.push(alarm);
+
+    console.log('📥 Nieuw postenalarm ontvangen:', alarm);
+
+    return res.status(200).json({ message: 'Alarm ontvangen', totaal: postenAlarms.length });
   }
 
   res.status(405).json({ error: 'Method not allowed' });
