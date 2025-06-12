9c548ded6e1bc0be72dc47b5dd3491cf5a0472cd
thijsken <41241417+thijsken@users.noreply.github.com>
2025-06-12 13:35:39 +0200

    update posten.js

--- api/posten.js
+++ api/posten.js
@@ -1,17 +1,7 @@
 export default async function handler(req, res) {
-  const { serverId } = req.query;
-
   if (req.method === 'GET') {
-    // Return posten data filtered by serverId if given
-    const posten = await getPostenFromDatabase(serverId); // implement this
-    return res.status(200).json(posten);
-  }
-
-  if (req.method === 'POST') {
-    // Save posten data (check auth, validate, etc)
-    const postenData = req.body;
-    await savePostenToDatabase(serverId, postenData); // implement this
-    return res.status(200).json({ message: "Posten opgeslagen" });
+    // TODO: haal posten op uit database
+    return res.status(200).json([]);
   }
 
   res.status(405).json({ error: 'Method not allowed' });
