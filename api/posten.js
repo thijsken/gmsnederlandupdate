2f64301e9d04f766d21d2783984d5fe74c159ae6
thijsken <41241417+thijsken@users.noreply.github.com>
2025-06-12 13:40:30 +0200

    update posten.js

--- api/posten.js
+++ api/posten.js
@@ -1,7 +1,13 @@
 export default async function handler(req, res) {
   if (req.method === 'GET') {
-    // TODO: haal posten op uit database
+    // haal posten op
     return res.status(200).json([]);
+  }
+  
+  if (req.method === 'POST') {
+    const postenData = req.body;
+    // verwerk postenData hier (opslaan in database)
+    return res.status(200).json({ success: true });
   }
 
   res.status(405).json({ error: 'Method not allowed' });
