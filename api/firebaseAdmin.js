4c0b689ef71e4038ae5261e92e2e1736d1c66d81
thijsken <41241417+thijsken@users.noreply.github.com>
2025-06-16 11:50:46 +0200

    update firebaseAdmin

--- api/firebaseAdmin.js
+++ api/firebaseAdmin.js
@@ -1,15 +1,15 @@
-// firebaseAdmin.js
-const admin = require('firebase-admin');
+// // firebaseAdmin.js
+// const admin = require('firebase-admin');
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://gmsnederland-3029e-default-rtdb.europe-west1.firebasedatabase.app',
-  });
-}
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: 'https://gmsnederland-3029e-default-rtdb.europe-west1.firebasedatabase.app',
+//   });
+// }
 
-const db = admin.database();
+// const db = admin.database();
 
-module.exports = { db };
+// module.exports = { db };
