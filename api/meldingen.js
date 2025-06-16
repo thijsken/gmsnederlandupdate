ccd303440f542fda4faf9a699096d7995ddf25da
thijsken <41241417+thijsken@users.noreply.github.com>
2025-06-16 10:39:35 +0200

    update meldingen api

--- api/meldingen.js
+++ api/meldingen.js
@@ -3,21 +3,20 @@
 const admin = require('firebase-admin');
 
 if (!admin.apps.length) {
-  // Maak de serviceAccount object aan vanuit env vars
-const serviceAccount = {
-  type: 'service_account',
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: JSON.parse(`"${process.env.FIREBASE_PRIVATE_KEY}"`),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-  token_uri: 'https://oauth2.googleapis.com/token',
-  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-};
+  const serviceAccount = {
+    type: 'service_account',
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  };
 
-console.log('RAW KEY:', process.env.FIREBASE_PRIVATE_KEY);
+console.log('RAW ENV:', process.env.FIREBASE_PRIVATE_KEY);
 console.log('KEY after replace:', process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'));
 console.log('KEY after JSON.parse:', JSON.parse(`"${process.env.FIREBASE_PRIVATE_KEY}"`));
 console.log('PRIVATE KEY FIRST 50 chars:', process.env.FIREBASE_PRIVATE_KEY.slice(0, 50));
@@ -26,13 +25,13 @@ console.log(JSON.stringify(process.env.FIREBASE_PRIVATE_KEY));
 
 const privateKey = JSON.parse(process.env.FIREBASE_PRIVATE_KEY);
 
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: privateKey,
-  }),
-});
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
+  });
 }
 
 const db = admin.database();
