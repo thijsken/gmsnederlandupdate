0a34f39d3f3c361d3af71960552ff6ed5cfb0f9d
thijsken <41241417+thijsken@users.noreply.github.com>
2025-06-16 13:17:08 +0200

    update firebaseAdmin

--- api/firebaseAdmin.js
+++ api/firebaseAdmin.js
@@ -38,8 +38,12 @@ if (!admin.apps.length) {
   }
 
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.FIREBASE_DATABASE_URL, // voor Realtime Database
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // <- HIER
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
   });
 }
 
