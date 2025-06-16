50fd98352d4038af42c84de2849472d954f9d078
thijsken <41241417+thijsken@users.noreply.github.com>
2025-06-16 12:43:52 +0200

    update firebaseAdmin

--- api/firebaseAdmin.js
+++ api/firebaseAdmin.js
@@ -1,51 +1,15 @@
 const admin = require('firebase-admin');
 
 if (!admin.apps.length) {
-  let serviceAccount;
-
-  if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-    try {
-      serviceAccount = JSON.parse(
-        process.env.FIREBASE_SERVICE_ACCOUNT.replace(/\\n/g, '\n')
-      );
-    } catch (err) {
-      console.error('Fout bij het parsen van FIREBASE_SERVICE_ACCOUNT:', err);
-      throw new Error('FIREBASE_SERVICE_ACCOUNT is ongeldig');
-    }
-  } else {
-    if (
-      !process.env.FIREBASE_PRIVATE_KEY ||
-      !process.env.FIREBASE_CLIENT_EMAIL ||
-      !process.env.FIREBASE_PROJECT_ID
-    ) {
-      throw new Error(
-        'Firebase configuratie ontbreekt. Stel FIREBASE_SERVICE_ACCOUNT of losse keys in.'
-      );
-    }
-
-    serviceAccount = {
-      type: 'service_account',
-      project_id: process.env.FIREBASE_PROJECT_ID,
-      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
-      client_id: process.env.FIREBASE_CLIENT_ID,
-      auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-      token_uri: 'https://oauth2.googleapis.com/token',
-      auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-      client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-    };
-  }
+  const serviceAccount = require('./confige/gmsnederland-3029e-firebase-adminsdk-fbsvc-c900bf64b5.json');
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.FIREBASE_DATABASE_URL, // voor Realtime Database
+    databaseURL: 'https://gmsnederland-3029e.firebaseio.com'  // let op juiste databaseURL voor Realtime DB
   });
 }
 
-// Firestore instantie
-const firestoreDb = admin.firestore();
-// Realtime Database instantie
-const realtimeDb = admin.database();
+const firestore = admin.firestore();
+const db = admin.database();  // db is hier de Realtime Database
 
-module.exports = { admin, firestoreDb, realtimeDb };
+module.exports = { admin, firestore, db };
